e38aedf18310c70d56185593bdded3b5e1f59f9e
silva3265 <silva3265@gmail.com>
2024-04-11 12:04:48 -0300

    Restart mais rápido da aplicação com DevTools

--- src/main/java/com/algaworks/algafood/MeuPrimeiroController.java
+++ src/main/java/com/algaworks/algafood/MeuPrimeiroController.java
@@ -4,13 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
+@Controller // Vai receber requisições web
 public class MeuPrimeiroController {
 
 	@GetMapping("/hello")
-	@ResponseBody
+	@ResponseBody // o retorno vai ser devolvida como resposta
 	public String hello() {
 		return "Hello!";
 	}
-	
+
 }
