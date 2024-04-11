bad6092bd1904859ac97d52812b48ef39116f316
silva3265 <silva3265@gmail.com>
2024-04-11 13:25:01 -0300

    O que é injeção de dependências?

---
+++ src/main/java/com/algaworks/di/AtivacaoClienteService.java
@@ -0,0 +1,17 @@
+package com.algaworks.di;
+
+public class AtivacaoClienteService {
+
+	private Notificador notificador;
+
+	public AtivacaoClienteService(Notificador notificador) {
+		this.notificador = notificador;
+	}
+
+	public void ativar(Cliente cliente) {
+		cliente.ativar();
+
+		this.notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+	}
+
+}
