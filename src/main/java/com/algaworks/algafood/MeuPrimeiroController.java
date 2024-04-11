952d4b50295fa831a3c7d980e0b7774bbb9ddaa7
silva3265 <silva3265@gmail.com>
2024-04-11 15:06:03 -0300

    Injetando dependências (beans Spring)

--- src/main/java/com/algaworks/algafood/MeuPrimeiroController.java
+++ src/main/java/com/algaworks/algafood/MeuPrimeiroController.java
@@ -4,13 +4,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.algaworks.algafood.di.modelo.Cliente;
+import com.algaworks.algafood.di.service.AtivacaoClienteService;
+
 @Controller // Controller é um component também
 public class MeuPrimeiroController {
+
+	private AtivacaoClienteService ativacaoClienteService;
+
+	public MeuPrimeiroController(AtivacaoClienteService ativacaoClienteService) {
+		this.ativacaoClienteService = ativacaoClienteService;
+		
+		System.out.println("Meu Primeiro Controller: " + ativacaoClienteService);
+	}
 
 	@GetMapping("/hello")
 	@ResponseBody
 	public String hello() {
+		
+		Cliente joao = new Cliente("Joao", "joao@xyz.com", "3499998888");
+		
+		ativacaoClienteService.ativar(joao);
 		return "Hello!";
 	}
-	
+
 }
--- src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -3,17 +3,23 @@ package com.algaworks.algafood.di.service;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.di.modelo.Cliente;
-import com.algaworks.algafood.di.notificacao.NotificadorEmail;
+import com.algaworks.algafood.di.notificacao.Notificador;
 
 @Component // vira um component Spring (Um Bean Spring)
 public class AtivacaoClienteService {
 
-	private NotificadorEmail notificador;
-	
+	private Notificador notificador;
+
+	public AtivacaoClienteService(Notificador notificador) {
+		this.notificador = notificador;
+		
+		System.out.println("AtivaçãoClienteService" + notificador);
+	}
+
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
 
 		notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
 	}
-	
+
 }
