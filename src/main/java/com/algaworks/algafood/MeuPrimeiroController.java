e97502522ecfbcf2e9aa3f5d429a1dbf609fa4d5
silva3265 <silva3265@gmail.com>
2024-04-22 21:22:38 -0300

    Usando @Configuration e @Bean para definir beans

--- src/main/java/com/algaworks/algafood/MeuPrimeiroController.java
+++ src/main/java/com/algaworks/algafood/MeuPrimeiroController.java
@@ -7,25 +7,25 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.algaworks.algafood.di.modelo.Cliente;
 import com.algaworks.algafood.di.service.AtivacaoClienteService;
 
-@Controller // Controller é um component também
+@Controller
 public class MeuPrimeiroController {
 
 	private AtivacaoClienteService ativacaoClienteService;
-
+	
 	public MeuPrimeiroController(AtivacaoClienteService ativacaoClienteService) {
 		this.ativacaoClienteService = ativacaoClienteService;
 		
-		System.out.println("Meu Primeiro Controller: " + ativacaoClienteService);
+		System.out.println("MeuPrimeiroController: " + ativacaoClienteService);
 	}
 
 	@GetMapping("/hello")
 	@ResponseBody
 	public String hello() {
+	    Cliente joao = new Cliente("João", "joao@xyz.com", "3499998888");
+	    
+	    ativacaoClienteService.ativar(joao);
 		
-		Cliente joao = new Cliente("Joao", "joao@xyz.com", "3499998888");
-		
-		ativacaoClienteService.ativar(joao);
 		return "Hello!";
 	}
-
+	
 }
--- src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -1,19 +1,16 @@
 package com.algaworks.algafood.di.service;
 
-import org.springframework.stereotype.Component;
-
 import com.algaworks.algafood.di.modelo.Cliente;
 import com.algaworks.algafood.di.notificacao.Notificador;
 
-@Component // vira um component Spring (Um Bean Spring)
 public class AtivacaoClienteService {
 
 	private Notificador notificador;
-
+	
 	public AtivacaoClienteService(Notificador notificador) {
 		this.notificador = notificador;
 		
-		System.out.println("AtivaçãoClienteService" + notificador);
+		System.out.println("AtivacaoClienteService: " + notificador);
 	}
 
 	public void ativar(Cliente cliente) {
@@ -21,5 +18,5 @@ public class AtivacaoClienteService {
 
 		notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
 	}
-
+	
 }
